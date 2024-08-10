17eb73f9485cf53a86fb7fca586c8ca64b41ba44
Adityaps52 <swainaditya921@gmail.com>
2024-08-10 07:01:26 +0530

    sum 2 number

--- src/Aditya.java
+++ src/Aditya.java
@@ -2,6 +2,9 @@
 
 class Aditya {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 5;
+        int c = (a+b);
+        System.out.println(c);
     }
 }
